556d81f6351b232d41953c599f9f409592d1b938
kyawkhantwin <kyawkhantwincc@gmail.com>
2025-01-29 19:55:16 +0630

    vefore pulll

---
+++ src/main/java/linearSearch/LinearSearch.java
@@ -0,0 +1,10 @@
+package linearSearch;
+
+public class LinearSearch {
+    int[] array;
+    int target;
+    public LinearSearch(int[] array,int target){
+        this.array = array;
+        this.target = target;
+    }
+}
